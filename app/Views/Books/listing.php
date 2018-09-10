2808c6880cbbf5b2972fb20df6c1427b6dd83fed
Werner van Randeraat <werner.van.randeraat@youngcoders.nl>
2018-09-10 12:58:56 +0200

    In Init.js de const updateBook aangepast. '&id=' toegevoegd en '+ bookId' toegevoegd. In update-book.js 'bookId' als 2e parameter meegegeven en de const bookId in update-book.php meegegeven.

--- app/Views/Books/listing.php
+++ app/Views/Books/listing.php
@@ -20,7 +20,7 @@
     <div class="col-md-3 card mb-2">
         <p class="text-center mt-2"><b>Comic of the month:</b></p>
         <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-        <div class="card-body">
+        <div class="card-body bg-comic">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
     </div>
